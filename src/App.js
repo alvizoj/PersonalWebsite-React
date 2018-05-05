5214ccd469301481de565d8315a9a3d79db9ba64
Colin Brown <kolivbrauv@gmail.com>
2018-05-04 20:39:46 -0700

    Fixed missing switch case default

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
                 return <AboutScreen/>;
             case "Contact":
                 return <ContactScreen/>;
+            default:
         }
     }
 
